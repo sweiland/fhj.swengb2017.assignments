b8708633241e21c6c1af4866476bb0b1662e52e8
sweiland <hello@sweiland.at>
2017-11-30 18:57:22 +0100

    All tests passed, Tree should be good to go.

--- tree/src/main/scala/at/fhj/swengb/assignments/tree/TreeAssignment.scala
+++ tree/src/main/scala/at/fhj/swengb/assignments/tree/TreeAssignment.scala
@@ -1,8 +1,8 @@
 package at.fhj.swengb.assignments.tree
 
+import java.util.Currency
 import javafx.scene.paint.Color
 
-import scala.math.BigDecimal.RoundingMode
 import scala.util.Random
 
 object Graph {
@@ -23,12 +23,14 @@ object Graph {
   /**
     * creates a random tree
     *
-    * @param root
+    * @param root - Start of the graph
     * @return
     */
   def randomTree(root: Pt2D): Tree[L2D] =
-    mkGraph(root, Random.nextInt(360), Random.nextDouble() * 150, Random.nextInt(7))
-
+    mkGraph(root,
+      Random.nextInt(360),
+      Random.nextDouble() * 150,
+      Random.nextInt(7))
 
   /**
     * Given a Tree of L2D's and a function which can convert any L2D to a Line,
@@ -39,8 +41,17 @@ object Graph {
     * @param convert a converter function
     * @return
     */
-  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] =  ???
+  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = {
+    def addElemToList(elem: Tree[A], list: Seq[A]): Seq[A] = {
+      elem match {
+        case Node(x) => list.seq :+ x
+        case Branch(left, right) =>
+          addElemToList(left, addElemToList(right, list))
+      }
+    }
 
+    addElemToList(tree, List()).reverse.map(convert)
+  }
 
   /**
     * Creates / constructs a tree graph.
@@ -61,19 +72,64 @@ object Graph {
               treeDepth: Int,
               factor: Double = 0.75,
               angle: Double = 45.0,
-              colorMap: Map[Int, Color] = Graph.colorMap): Tree[L2D] = ???
+              colorMap: Map[Int, Color] = Graph.colorMap): Tree[L2D] = {
 
+    require(treeDepth >= 0 && treeDepth <= (colorMap.size - 1))
+
+    def createSubTree(leaf: Node[L2D],
+                      factor: Double,
+                      angle: Double,
+                      color: Color): Branch[L2D] = {
+      val nodeLeft = Node(leaf.value.left(factor, angle, color))
+      val nodeRight = Node(leaf.value.right(factor, angle, color))
+
+      Branch(leaf, Branch(nodeLeft, nodeRight))
+    }
+
+    def createTree(currTree: Tree[L2D],
+                   depth: Int,
+                   maxDepth: Int): Tree[L2D] = {
+      if (depth == maxDepth)
+        currTree
+      else {
+        def addNewLevel(tree: Tree[L2D], currLevel: Int): Branch[L2D] = {
+          tree match {
+            case Node(root) =>
+              createSubTree(Node(root), factor, angle, colorMap(currLevel))
+            case Branch(Node(root), Branch(Node(left), Node(right))) =>
+              val newSubtreeLeft =
+                createSubTree(Node(left), factor, angle, colorMap(currLevel))
+              val newSubtreeRight =
+                createSubTree(Node(right), factor, angle, colorMap(currLevel))
+              Branch(Node(root), Branch(newSubtreeLeft, newSubtreeRight))
+            case Branch(Node(root), Branch(left, right)) =>
+              Branch(Node(root),
+                Branch(addNewLevel(left, depth + 1),
+                  addNewLevel(right, depth + 1)))
+            case Branch(_, _) => ???
+          }
+        }
+
+        createTree(addNewLevel(currTree, depth), depth + 1, maxDepth)
+      }
+    }
+
+    val rootNode: Tree[L2D] = Node(
+      L2D(start, initialAngle, length, colorMap(0)))
+
+    treeDepth match {
+      case 0 => rootNode
+      case _ => createTree(rootNode, 0, treeDepth)
+    }
+  }
 }
-
-
-
 
 object L2D {
 
   import MathUtil._
 
   /**
-    * Given a startpoint, an angle and a length the endpoint of the line
+    * Given a startpoint, an angle and a length, the endpoint of the line
     * is calculated and finally a L2D class is returned.
     *
     * @param start the startpoint
@@ -82,13 +138,15 @@ object L2D {
     * @param color the color
     * @return
     */
-  def apply(start: Pt2D, angle: AngleInDegrees, length: Double, color: Color): L2D = {
+  def apply(start: Pt2D,
+            angle: AngleInDegrees,
+            length: Double,
+            color: Color): L2D = {
     val angleInRadiants = toRadiants(angle)
     val end = Pt2D(start.x + length * Math.cos(angleInRadiants),
       start.y + length * Math.sin(angleInRadiants)).normed
     new L2D(start, end, color)
   }
-
 
 }
 
@@ -101,18 +159,17 @@ case class L2D(start: Pt2D, end: Pt2D, color: Color) {
   lazy val angle = {
     assert(!((xDist == 0) && (yDist == 0)))
     (xDist, yDist) match {
-      case (x, 0) if x > 0 => 0
-      case (0, y) if y > 0 => 90
-      case (0, y) if y < 0 => 270
-      case (x, 0) if x < 0 => 180
+      case (x, 0) if x > 0          => 0
+      case (0, y) if y > 0          => 90
+      case (0, y) if y < 0          => 270
+      case (x, 0) if x < 0          => 180
       case (x, y) if x < 0 && y < 0 => Math.atan(y / x) * 180 / Math.PI + 180
       case (x, y) if x < 0 && y > 0 => Math.atan(y / x) * 180 / Math.PI + 180
       case (x, y) if x > 0 && y < 0 => Math.atan(y / x) * 180 / Math.PI + 360
-      case (x, y) => Math.atan(y / x) * 180 / Math.PI
+      case (x, y)                   => Math.atan(y / x) * 180 / Math.PI
     }
   }
 
-  /** the line length **/
   lazy val length: Double = {
     Math.sqrt(xDist * xDist + yDist * yDist)
   }
@@ -125,6 +182,4 @@ case class L2D(start: Pt2D, end: Pt2D, color: Color) {
     L2D(end, angle + deltaAngle, length * factor, c)
   }
 
-
 }
-
